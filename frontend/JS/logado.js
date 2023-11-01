e7adf4a17257f50bdb51a506be9cebfedced82bc
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-10-31 22:41:32 -0300

    Implement_Functionality

--- frontend/JS/logado.js
+++ frontend/JS/logado.js
@@ -1,4 +1,12 @@
-//document.addEventListener("DOMContentLoaded", function() {
 const email = localStorage.getItem('email');
 document.getElementById('texto').textContent = email;
-//})    
+
+function displayUser(){
+    let display = document.getElementById("userMenu").style.display;
+    console.log(display);
+    if(display == "none"){
+        display.style.display = "block";
+    }else{
+        display.style.display = "none";
+    }
+}
