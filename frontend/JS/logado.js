2d7a6bf245cf35b6a22acf20395eab7cef554356
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-11-02 20:51:14 -0300

    changes_frontend

--- frontend/JS/logado.js
+++ frontend/JS/logado.js
@@ -2,11 +2,14 @@ const email = localStorage.getItem('email');
 document.getElementById('texto').textContent = email;
 
 function displayUser(){
-    let display = document.getElementById("userMenu").style.display;
-    console.log(display);
-    if(display == "none"){
-        display.style.display = "block";
-    }else{
+    let display = document.getElementById("userMenu");
+    let icon = document.getElementById("iconUser");
+    const controle = display.style.display;
+    if(controle == "block"){
         display.style.display = "none";
+        icon.style.color = "white";
+    }else{
+        display.style.display = "block";
+        icon.style.color = "black";
     }
 }
