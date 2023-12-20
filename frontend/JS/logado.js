21c2a5cb9f272f06ba6ab11f895d4279dec1f97f
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-12-20 11:25:14 -0300

    implementação do carrinho

--- frontend/JS/logado.js
+++ frontend/JS/logado.js
@@ -1,3 +1,52 @@
+let itens = []
+
+function adicionaNoCarrinho(productId){
+    fetch('http://localhost:3000/products',{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+          }
+    })
+    .then(async (results)=>  {
+        const products = await results.json();
+        const produtoSelecionado = products.find(product => product.id === productId);
+
+        if (produtoSelecionado) {
+            let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+
+            carrinho.push(produtoSelecionado);
+
+            localStorage.setItem('carrinho', JSON.stringify(carrinho));
+
+            console.log('Produto adicionado ao carrinho:', carrinho);
+
+        } else {
+            console.log('Produto não encontrado');
+        }
+    });
+}
+
+function listarCarrinho(){
+    var carrinho = localStorage.getItem('carrinho')
+    console.log(carrinho)
+}
+
+function removerDoCarrinho(productId){
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+
+    const index = carrinho.findIndex(item => item.id === productId);
+
+    if (index !== -1) {
+        carrinho.splice(index, 1);
+
+        localStorage.setItem('carrinho', JSON.stringify(carrinho));
+
+        console.log('Produto removido do carrinho:', carrinho);
+    } else {
+        console.log('Produto não encontrado no carrinho');
+    }
+}
+
 function displayUser(){
     let display = document.getElementById("userMenu");
     let icon = document.getElementById("iconUser");
